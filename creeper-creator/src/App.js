5c45449d3a32bf81ad039f97c46c3d678b4f07a6
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-19 14:15:45 -0500

    added creeper photo

--- creeper-creator/src/App.js
+++ creeper-creator/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import creeper from "./creeper.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={creeper} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </>
   );
 }
 
